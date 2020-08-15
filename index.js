78bd4dc70dc4cc225b99530071e8e453c98b6379
sara01romao <sara-romao@live.com>
2020-08-14 23:19:00 -0300

    .

---
+++ index.js
@@ -0,0 +1,23 @@
+// Criar um programa que calcula a média das notas entre os alunos e envia msg do cálculo da média
+
+const aluno1= "Lua";
+const aluno2 = "Sol";
+
+const n1 = 10;
+const n2 = 6.5;
+
+const media = (n1+n2)/2;
+
+// console.log(media);
+//-------------------------------
+
+//CONDICIONAL
+
+//Se a média for maior que 5, parabenizar a turma
+
+if(media>5){
+    console.log(`A média foi ${media}  parabens`)
+}else{
+    console.log(`A MÉDIA foi ${media}`)
+}
+
