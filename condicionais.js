a658c57ca9f5faa932ae6582483c6c25eed85388
sara01romao <sara-romao@live.com>
2020-08-22 22:05:47 -0300

    .

---
+++ condicionais.js
@@ -0,0 +1,16 @@
+//Condicional- IF e ELSE
+
+
+const n1 = 10;
+const n2 = 6.5;
+
+const media = (n1+n2)/2;
+
+
+//exemplo: Se a média for maior que 5, parabenizar a aluno
+
+if(media>5){
+    console.log(`A média foi ${media}  parabéns`)
+}else{
+    console.log(`A média foi ${media} tem que melhorar`)
+}
---
+++ operadores.js
@@ -0,0 +1,98 @@
+//--------------------OPERADORES------------------------ 
+
+
+/**
+ * ----------//RELACIONAIS//---------------
+ * > MAIOR QUE
+ * < MENOR QUE
+ * >= MAIOR IGUAL A
+ * <= MENOR IGUAL A
+ * 
+ * ex:
+ * const a= 5;
+   const b= 6;
+
+    console.log(a>b)//fase
+    console.log(b>=a)//true
+
+ */
+
+
+
+/**
+ * ----------//IGUALDADE//---------------
+ * == IGUAL A
+ * != DIFERENTE DE
+ * === IGUAL E DO MESMO TIPO
+ * !== DIFERENTE INCLUSIVE DO TIPO
+ * 
+ * ex:
+ * const a= 10;
+   const b= "10";
+
+    console.log(a==b)//true
+    console.log(b===a)//false 
+    console.log(b!=a)//false
+    console.log(a!==b)//true
+
+
+    ex:
+    const a= 10;
+    const b= "10";
+
+ console.log(a==b)//true
+ console.log(b===a)//false 
+ console.log(b!=a)//false
+ console.log(a!==b)//true
+
+ */
+
+
+
+
+/**
+ * ----------//LÓGICO//---------------
+ * 
+ * Usadas geralmente para criar expressões (true ou false)
+ * 
+ * && "E"
+ * || "OU"
+ * !  "NÂO"
+ * 
+ * ex:
+ * const a =5;
+   const b ='5';
+
+   console.log( a==b && a !==b)//true
+   console.log(a==b || a != b)//true
+   console.log(!( a==b && a !==b))//false
+
+
+ */
+
+ 
+
+
+
+
+/**
+ * ----------//ARITMÉTICO//---------------
+ *  * MULTIPLICAÇÃO 
+ *  / DIVISÃO
+ *  % RESTO DA DIVISÃO
+ *  + ADIÇÃO
+ *  - SUBTRAÇÃO
+
+ * 
+ * ex:
+ console.log(6*2);//12
+ console.log(6/2);//3
+ console.log(6%2);//0
+ console.log(6+2);//8
+ console.log(6-2);//4
+
+ */
+
+ 
+ 
+
