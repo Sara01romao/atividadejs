1817b090c6ef9100b04ab775099acd736cf713bb
sara01romao <sara-romao@live.com>
2020-09-07 22:29:57 -0300

    .

---
+++ 3-objetos.js
@@ -0,0 +1,15 @@
+//Declarar um objeto  exemplo:
+
+
+const aluno1 ={
+    nome: "Lua",
+    idade: 16
+}
+
+const aluno2 ={
+    nome: "Sol",
+    idade: 20
+}
+
+
+console.log(aluno1.nome, aluno2.nome)
---
+++ 4-array.js
@@ -0,0 +1,28 @@
+//Estrutura para declar um array/ vetor
+
+
+
+const nomes=["Lua", "Sol", "Terra"];
+
+console.log(nomes);
+
+console.log(nomes[2])
+
+
+//array com objetos exemplo
+
+const ninja=[
+    {
+        nome:"Naruto",
+        vila: "Folha",
+        nota: 8
+    },
+
+    {
+        nome:"Gaara",
+        vila: "Areia",
+        nota: 9
+    }
+]
+
+console.log(ninja[1])
